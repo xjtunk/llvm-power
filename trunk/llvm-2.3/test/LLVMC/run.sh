0588068f4b84cd61e0bcd83df53eff29b5031bda
jonathan.kron <jonathan.kron@8dad9148-8cc0-11dd-a9db-710176c1da70>
2008-09-27 19:28:35 +0000

    Added debug mode support for run.sh.

--- trunk/llvm-2.3/test/LLVMC/run.sh
+++ trunk/llvm-2.3/test/LLVMC/run.sh
@@ -1,2 +1,5 @@
 llvm-gcc -emit-llvm -c hello.c -o hello.bc
+# If you are using debug build, use this one
+opt -load=../../Debug/lib/LLVMHello.dylib -hello hello.bc
+# else use the standard one
 opt -load=../../Release/lib/LLVMHello.so -hello hello.bc
