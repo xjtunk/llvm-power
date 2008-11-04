1539cfbab4673b46d76720c5e40454d34f050ad2
ahmadsharif <ahmadsharif@8dad9148-8cc0-11dd-a9db-710176c1da70>
2008-11-04 05:23:22 +0000

    Loading of profiles is now working from inside X86PowerPass.cpp.

--- trunk/llvm-2.3/test/LLVMC/lli.sh
+++ trunk/llvm-2.3/test/LLVMC/lli.sh
@@ -1,2 +1,3 @@
-lli -load ../../Debug/lib/profile_rt.so -print-machineinstrs -x86-asm-syntax=intel hello.bc
+###lli -load ../../Debug/lib/profile_rt.so -print-machineinstrs -x86-asm-syntax=intel hello.bc
+lli -load ../../Debug/lib/profile_rt.so -x86-asm-syntax=intel hello.bc
 
