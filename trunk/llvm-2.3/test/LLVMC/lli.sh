b673a3fed8938d448dbc25208286f7d60d69e86b
ahmadsharif <ahmadsharif@8dad9148-8cc0-11dd-a9db-710176c1da70>
2008-11-09 00:46:49 +0000

    Added ISA support for gating instruction. Implemented basic trace power optimization function.

--- trunk/llvm-2.3/test/LLVMC/lli.sh
+++ trunk/llvm-2.3/test/LLVMC/lli.sh
@@ -1,5 +1,5 @@
 ###lli -load ../../Debug/lib/profile_rt.so -print-machineinstrs -x86-asm-syntax=intel hello.bc
 ###lli -load ../../Debug/lib/profile_rt.so -x86-asm-syntax=intel hello.bc
 ###lli -load ../../Debug/lib/profile_rt.so -x86-asm-syntax=intel tracetest.bc
-lli -load ../../Debug/lib/profile_rt.so -x86-asm-syntax=intel tracetest1.bc
+lli -load ../../Debug/lib/profile_rt.so -x86-asm-syntax=intel loop.bc
 
