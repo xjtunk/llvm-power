d5e65b3d20eb0ea2e8665fcd7163d38f67a4c495
ahmadsharif <ahmadsharif@8dad9148-8cc0-11dd-a9db-710176c1da70>
2008-12-04 01:28:31 +0000

    The code is NOT working because of analysis usage requirement.

--- trunk/llvm-2.3/lib/Transforms/Instrumentation/JITPass.cpp
+++ trunk/llvm-2.3/lib/Transforms/Instrumentation/JITPass.cpp
@@ -29,7 +29,7 @@ reads and runs additional passes on the function.
 
 //// Brooks
 //// including loopinfo.h to use the iterator for loops
-#include "llvm/CodeGen/LoopInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 using namespace std;
 using namespace llvm;
@@ -70,42 +70,142 @@ public:
     ReturnToJITPass() : ModulePass((intptr_t) & ID)
     {
     }
+
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+      // added for loop info
+      AU.addRequired<LoopInfo>(); // where does AU come from?
+    }
+
+    void AddTrampoline(BasicBlock * bb, unsigned int threshold)
+    {
+      Function * f;
+      Module * M;
+      BasicBlock * trampolineBB;
+      
+      assert(bb);
+      f=bb->getParent();
+      assert(f);
+      M=f->getParent();
+      assert(M);
+      Constant * one=ConstantInt::get(Type::Int32Ty, 1);
+      Constant * zero=ConstantInt::get(Type::Int32Ty, 0);
+      Constant *hot_threshold = ConstantInt :: get(Type :: Int32Ty, threshold);
+      BasicBlock * newEntry=BasicBlock::Create("new_entry", f, bb);
+      Value *counter=new GlobalVariable(zero->getType(), false, GlobalValue::InternalLinkage, hot_threshold, f->getName()+"_execution_count", M);
+      Value * result=BinaryOperator::createSub(new LoadInst(counter, "",newEntry), one, "", newEntry);
+      new StoreInst(result, counter, newEntry);
+      trampolineBB=CreateBreakBB(bb, (void*)InsertMessage, (void*)bb->getParent());
+      assert(trampolineBB);
+      BranchInst::Create(trampolineBB, bb, new ICmpInst(ICmpInst::ICMP_EQ, result, zero, "", newEntry), newEntry);
+
+#if 0
+        BasicBlock *old_entry = f->begin(),
+                   *new_entry = BasicBlock :: Create("", f, old_entry),
+                   *hot_block = BasicBlock :: Create("", f, old_entry);
+
+        Constant *one = ConstantInt :: get(Type :: Int32Ty, 1),
+                 *zero = ConstantInt :: get(Type :: Int32Ty, 0);
+
+        Value *counter =
+          new GlobalVariable(zero->getType(), false, GlobalValue::InternalLinkage, hot_threshold, f->getName() + "_execution_count", &M);
+
+
+        Value *result = BinaryOperator::createSub(new LoadInst(counter, "", new_entry), one, "", new_entry);
+        new StoreInst(result, counter, new_entry);
+
+        Constant *message = ConstantArray :: get(f->getName() + " is cold\n");
+        GlobalVariable *message_global = new GlobalVariable(message->getType(), true, GlobalValue::InternalLinkage, message, "", &M);
+        message = ConstantExpr::getGetElementPtr(message_global, zeros, 2);
+        CallInst::Create(printf_function, message, "", new_entry);
+
+        BranchInst :: Create(hot_block, old_entry, new ICmpInst(ICmpInst::ICMP_EQ, result, zero, "", new_entry), new_entry);
+#endif
+    }
+
+    // Creates a break-out basic block to break out of the JIT into the runtime.
+    // It assumes that the runtime will be called with a function of this type:
+    // void breakfn(void * param);
+    BasicBlock* CreateBreakBB(BasicBlock * bb, void * fn, void * param)
+    {
+      Function * f=bb->getParent();
+      BasicBlock * breakBB=BasicBlock::Create("", f, bb);
+      // parameter types of our break function
+      vector<const Type*> parameter_types;
+      // actual arguments passed to the function.
+      vector<Value*> args;
+      // temp value used
+      Constant* C=NULL;
+      // Pointer to our break function.
+      Value* breakfn;
+      if(sizeof(void*)==4)
+      {
+        // 32-bit machine.
+        C=ConstantInt::get(Type::Int32Ty, (int)(intptr_t)f);
+        parameter_types.push_back(PointerType::get(Type::Int32Ty, 0));
+        C=ConstantExpr::getIntToPtr(C, parameter_types[0]);
+        args.push_back(C);
+        breakfn=ConstantExpr::getIntToPtr(ConstantInt::get(Type::Int32Ty, (uint32_t)InsertMessage),
+            PointerType::get(FunctionType::get(Type::VoidTy, parameter_types, false), 0));
+      }
+      else
+      {
+        // 64-bit machine.
+        assert(0);
+      }
+      CallInst::Create(breakfn, args.begin(), args.end(), "", breakBB);
+      BranchInst::Create(bb, breakBB);
+      return breakBB;
+
+#if 0
+        vector <const Type *> parameter_types(1);
+        SmallVector<Value*, 8> Args;
+        Function * fptr=f;
+        Constant *C = 0;
+        if (sizeof(void*) == 4)
+        {
+          C = ConstantInt::get(Type::Int32Ty, (int)(intptr_t)(fptr));
+          parameter_types[0] = PointerType :: get(Type :: Int32Ty, 0);
+        }
+        else
+        {
+          C = ConstantInt::get(Type::Int64Ty, (intptr_t)(fptr));
+          parameter_types[0] = PointerType :: get(Type :: Int64Ty, 0);
+        }
+        C = ConstantExpr::getIntToPtr(C, parameter_types[0]);  // Cast the integer to pointer
+        Args.push_back(C);
+
+///        parameter_types[0] = PointerType :: get(Type :: Int8Ty, 0);
+
+        Constant *fname = ConstantArray :: get(f->getName());
+        GlobalVariable *fname_global = new GlobalVariable(fname->getType(), true, GlobalValue::InternalLinkage, fname, "", &M);
+        fname = ConstantExpr::getGetElementPtr(fname_global, zeros, 2);
+        Value *insert_message_function = ConstantExpr :: getIntToPtr(ConstantInt :: get(Type :: Int32Ty, (uint32_t)InsertMessage),
+                   PointerType :: get(FunctionType::get(Type :: VoidTy, parameter_types, false), 0));
+///        CallInst :: Create(insert_message_function, fname, "", hot_block);
+        CallInst::Create(insert_message_function, Args.begin(), Args.end(), "", hot_block);
+        BranchInst :: Create(old_entry, hot_block);
+#endif
+    }
     bool runOnModule(Module &M)
     {
-      return true;
-      CreatePrintfDeclaration(M);
-
-       Constant *zeros[] =
-       {
-          ConstantInt::getNullValue(Type :: Int32Ty),
-          ConstantInt::getNullValue(Type :: Int32Ty)
-       };
-       Constant *hot_threshold = ConstantInt :: get(Type :: Int32Ty, 20),
-                *printf_function = M.getFunction("printf");
-       assert(printf_function!=0);
-
       for (Module::iterator f = M.begin(); f != M.end(); ++f)
       {
         if (f->isDeclaration())
           continue;
 
-        BasicBlock *old_entry = f->begin(),
-                   *new_entry = BasicBlock :: Create("", f, old_entry),
-                   *hot_block = BasicBlock :: Create("", f, old_entry);
+        BasicBlock *old_entry = f->begin();
 
 		//// Brooks
 		//// Call Ahmad's routine at the beginning of each function
-		AddTrampoline(old_entry, 1/*threshold*/);
+		AddTrampoline(old_entry, 10/*threshold*/);
 
 		//// Brooks
 		//// Also, call Ahmad's routine at the beginning of each loop in the function
-		AnalysisUsage AU;
-		AU.addRequired<LoopInfo>(); // where does AU come from?
 		LoopInfo * LI; 
 		LI = &getAnalysis<LoopInfo>(); // does this work? (if not, try LoopInfo::getAnalysis
-		for (LoopInfo::iterator lii = LI->begin, liend = LI->end(); lii != liend; ++lii) {
+		for (LoopInfo::iterator lii = LI->begin(), liend = LI->end(); lii != liend; ++lii) {
 			Loop * currloop = *lii;
-			AddTrampoline(currloop->getHeader(), 1/*threshold*/);
+			AddTrampoline(currloop->getHeader(), 10/*threshold*/);
 		}
 
       }
